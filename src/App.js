6364f645489bcc371d05e753f249c9c7d40a78f8
AlagusuryaPeriyakaruppan <alagup1405@gmail.com>
2024-08-20 23:21:59 +0530

    Pagination-initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const MAX_LIMIT = 20;
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const getProducts = async () => {
+    const response = await fetch(
+      `https://dummyjson.com/products?limit=10&skip=${currentPage * 10 - 10}`
+    );
+    const data = await response.json();
+
+    if (data && data.products) {
+      setProducts(data.products);
+      setTotalPages(data.total / MAX_LIMIT);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, [currentPage]);
+
+  const handlePageChange = (selectedPage) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage !== currentPage &&
+      selectedPage <= totalPages
+    ) {
+      setCurrentPage(selectedPage);
+    }
+  };
+
+  const handlePrevious = () => {
+    setCurrentPage((currentPage) => currentPage - 1);
+  };
+
+  const handleNext = () => {
+    setCurrentPage((currentPage) => currentPage + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.length &&
+        products.map((product) => {
+          return (
+            <div key={product.id} className="pagination__card">
+              <img
+                src={product.images[0]}
+                alt={product.title}
+                width={400}
+                height={300}
+              />
+              <span>{product.title}</span>
+            </div>
+          );
+        })}
+      {products.length && (
+        <div>
+          {currentPage > 1 && (
+            <button onClick={handlePrevious}>Previous</button>
+          )}
+          {[...Array(Math.ceil(totalPages))].map((_, i) => (
+            <span key={i} onClick={() => handlePageChange(i + 1)}>
+              {i + 1}
+            </span>
+          ))}
+
+          {currentPage < totalPages && (
+            <button onClick={handleNext}>Next</button>
+          )}
+        </div>
+      )}
     </div>
   );
 }
